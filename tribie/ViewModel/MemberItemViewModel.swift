51b8cb3fad027f85616709f2a16691088a2cacc2
Arnold Sidiprasetija <arnolds1809@gm>
2022-08-30 22:17:56 +0700

    Add TripView and TripViewModel

--- tribie/ViewModel/MemberItemViewModel.swift
+++ tribie/ViewModel/MemberItemViewModel.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+
+struct Trip: Hashable, Codable, Identifiable {
+    var id: String?
+    var description: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+}
+
+struct TripMember: Hashable, Codable, Identifiable {
+    var id: String?
+    var tripId: String?
+    var userId: String?
+    var name: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+}
+
+struct Transaction: Hashable, Codable, Identifiable {
+    var id: String?
+    var tripId: String? 
+    var title: String?
+    var description: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var userPaidId: String?
+}
+
+struct TransactionItem: Hashable, Codable, Identifiable {
+    var id: String?
+    var title: String?
+    var description: String?
+    var price: Int?
+    var quantity: Int?
+    var createdAt: Date?
+    var updatedAt: Date?
+}
+
+struct TransactionExpenses: Hashable, Codable, Identifiable {
+    var id: String?
+    var itemId: String?
+    var transactionId: String?
+    var tripMemberId: String?
+    var quantity: Int?
+    var createdAt: Date?
+    var updatedAt: Date?
+}
+
+struct TransactionSettlement: Hashable, Codable, Identifiable {
+    var id: String?
+    var itemId: String?
+    var tripMemberId: String?
+    var transactionId: String?
+    var nominal: Int?
+    var createdAt: Date?
+    var updatedAt: Date?
+}
