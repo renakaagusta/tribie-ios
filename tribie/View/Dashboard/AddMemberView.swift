7b66d1c6bf6a89143507f8cd658bd78f654dda78
renakaagusta <renakaagusta28@gmail.com>
2022-09-12 21:59:52 +0700

    Add trip form view

--- tribie/View/Dashboard/AddMemberView.swift
+++ tribie/View/Dashboard/AddMemberView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct AddMemberView: View {
+    
+    //for Modal Environment
+    @Environment(\.presentationMode) var presentationMode
+
+    //variable for modal
+    @State var searchMember: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        NavigationView {
+            VStack {
+
+                Text("(searchMember)")
+                    .searchable(text: $searchMember)
+
+                AppCaption1(text: "Find members using Apple ID")
+
+                Spacer()
+
+            } //VStack
+            .navigationTitle("Add Members")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                //for leading navigation bar items
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        //action
+                        presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        AppBody1(text: "Done", color: Color.primaryColor, fontWeight: .bold)
+                    })
+                }
+            } //toolbar
+        } //Navigation View
+
     }
 }
 
