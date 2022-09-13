c9bad0685dae46f984fdb7cf300cc1bfdb393514
Arnold Sidiprasetija <arnolds1809@gm>
2022-09-13 17:35:41 +0700

    Solve image logo file and AuthView

--- tribie/View/Dashboard/TransactionListView.swift
+++ tribie/View/Dashboard/TransactionListView.swift
@@ -14,52 +14,51 @@ struct TransactionListView: View {
     @ObservedObject var transactionViewModel: TransactionListViewModel = TransactionListViewModel()
     
     var body: some View {
-            VStack {
-                if (transactionViewModel.state == AppState.Loading) {
-                    AppLoading()
-                }
-                if (transactionViewModel.state == AppState.Empty) {
-                    AppBody1(text: "Empty")
-                }
-                if (transactionViewModel.state == AppState.Error) {
-                    AppBody1(text: "Error bang")
-                }
-                if (transactionViewModel.state == AppState.Exist) {
-                    ScrollView {
-                        VStack(alignment: .leading){
-                            HStack{
-                                AppFootnote(text: "Active Trip", fontWeight: .regular, textAlign: .leading)
-                                    .padding(.horizontal)
-                                Spacer()
-                            }
-                            AppTitle1(text: "Liburan Tribie", color: Color.primaryColor, fontWeight: .semibold, fontSize: 20).padding(.horizontal)
-                            Spacer()
-                            AppHeader(text: "Drafts", textAlign: .leading)
-                            AppCaption1(text: "List of transaction you haven’t manage yet")
-                        } //VStack
-                        HStack {
-                            AppTitle1(text: "Unsplitted Bill", fontWeight: .bold, textAlign: .leading)
-                                .padding(.vertical)
-                            Spacer()
-                        }
-                        
-                        if(transactionViewModel.transactionList != nil && transactionViewModel.transactionExpensesList != nil) {
-                            ForEach(transactionViewModel.transactionList!) { transaction in
-                                NavigationLink(destination: SplitBillView(tripId: tripId, transactionId: transaction.id!, formState: SplitbillState.InputTransactionItem)) {
-                                    RecentTransactionCard(memberPaid: transactionViewModel.getUserPaid(userPaidId: transaction.userPaidId ?? "").name!, title: transaction.title ?? "", date: "24",time: "9.24", total: transaction.grandTotal ?? 0)
-                                }
-                            }
-                        } else {
-                            AppCaption1(text: "You’re done. No unsplitted bill.")
-                        }
-                        
-                        if(transactionViewModel.transactionList == nil || transactionViewModel.transactionExpensesList == nil) {
-                            AppLoading()
-                        }
-                    }.padding()
-                }
-                Spacer()
+        VStack {
+            if (transactionViewModel.state == AppState.Loading) {
+                AppLoading()
             }
+            if (transactionViewModel.state == AppState.Empty) {
+                AppBody1(text: "Empty")
+            }
+            if (transactionViewModel.state == AppState.Error) {
+                AppBody1(text: "Error bang")
+            }
+            if (transactionViewModel.state == AppState.Exist) {
+                ScrollView {
+                    VStack(alignment: .leading){
+                        HStack{
+                            AppFootnote(text: "Active Trip", fontWeight: .regular, textAlign: .leading)
+                                .padding(.horizontal)
+                            Spacer()
+                        }
+                        AppTitle1(text: "Liburan Tribie", color: Color.primaryColor, fontWeight: .semibold, fontSize: 20).padding(.horizontal)
+                        Spacer()
+                        AppHeader(text: "Drafts", textAlign: .leading)
+                        AppCaption1(text: "List of transaction you haven’t manage yet")
+                    } //VStack
+                    HStack {
+                        AppTitle1(text: "Unsplitted Bill", fontWeight: .bold, textAlign: .leading)
+                            .padding(.vertical)
+                        Spacer()
+                    }
+                    
+                    if(transactionViewModel.transactionList != nil && transactionViewModel.transactionExpensesList != nil) {
+                        ForEach(transactionViewModel.transactionList!) { transaction in
+                            NavigationLink(destination: SplitBillView(tripId: tripId, transactionId: transaction.id!, formState: SplitbillState.InputTransactionItem)) {
+                                RecentTransactionCard(memberPaid: transactionViewModel.getUserPaid(userPaidId: transaction.userPaidId ?? "").name!, title: transaction.title ?? "", date: "24",time: "9.24", total: transaction.grandTotal ?? 0)
+                            }
+                        }
+                    } else {
+                        AppCaption1(text: "You’re done. No unsplitted bill.")
+                    }
+                    
+                    if(transactionViewModel.transactionList == nil || transactionViewModel.transactionExpensesList == nil) {
+                        AppLoading()
+                    }
+                }.padding()
+            }
+            Spacer()
         }
         .onAppear {
             transactionViewModel.fetchData(tripId: tripId)
